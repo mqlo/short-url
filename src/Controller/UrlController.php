d7b8744fdaf6dd4282eef4b107d15dd7c3cb9741
Константин Миронов <mirronovko@gmail.com>
2020-11-04 15:25:41 +0500

    Finish

--- src/Controller/UrlController.php
+++ src/Controller/UrlController.php
@@ -2,8 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\Url\Url;
-use App\Repository\UrlRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -29,26 +27,12 @@ final class UrlController extends AbstractController
                 $handler->handle($command);
                 return $this->redirectToRoute('home');
             } catch (\DomainException $e) {
-                dd($e->getMessage());
-//                $this->errors->handle($e);
-//                $this->addFlash('error', $e->getMessage());
+                $this->addFlash('error', $e->getMessage());
             }
         }
 
         return $this->render('url/new.html.twig', [
             'form' => $form->createView(),
         ]);
-    }
-
-    /**
-     * @Route("/{id}", name="url_delete", methods={"DELETE"})
-     */
-    public function delete(Request $request, Url $url): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$url->getId(), $request->request->get('_token'))) {
-            dd("delete url id {$url->getId()->getValue()}");
-        }
-
-        return $this->redirectToRoute('/');
     }
 }
